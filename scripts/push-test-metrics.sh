b3873b5f86380d91f407ddc1da7af998e62d70c5
shivang20 <shivangthakurqa@gmail.com>
2026-03-14 22:29:43 +0530

    pushgateway update

--- scripts/push-test-metrics.sh
+++ scripts/push-test-metrics.sh
@@ -3,7 +3,14 @@
 PASSED=1
 FAILED=0
 
-cat <<EOF | curl --data-binary @- $PUSHGATEWAY_URL/metrics/job/automation
-automation_tests_passed{module="$MODULE",env="$ENV",suite="$SUITE"} $PASSED
-automation_tests_failed{module="$MODULE",env="$ENV",suite="$SUITE"} $FAILED
+if [ "$BUILD_RESULT" != "SUCCESS" ]; then
+FAILED=1
+PASSED=0
+fi
+
+cat <<EOF | curl --data-binary @- $PUSHGATEWAY_URL/metrics/job/automation_tests
+
+automation_tests_passed{module="$MODULE",env="$ENV",scope="$SUITE",browser="$BROWSER"} $PASSED
+automation_tests_failed{module="$MODULE",env="$ENV",scope="$SUITE",browser="$BROWSER"} $FAILED
+
 EOF
